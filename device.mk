5a9ad311d374bfa2f6354fa53c9d4ee1c079c100
Arian <arian.kulmer@web.de>
2022-04-14 00:12:50 +0200

    TP1803: Don't explicitly build fs_config_files
    The fs_config_files for each partition is already added to
    PRODUCT_PACKAGES by base_{product,system,system_ext,vendor}.mk.
    
    Change-Id: Ifbadacd5cb20beca70ed8b3accb3baf86a989c92

--- device.mk
+++ device.mk
@@ -17,10 +17,6 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
-# AID/fs configs
-PRODUCT_PACKAGES += \
-    fs_config_files
-
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService-Soong \
