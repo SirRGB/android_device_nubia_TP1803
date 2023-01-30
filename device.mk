3e90427ef77d5d2b25cd6575ef23c0be189aa27f
SirRGB <sirrgb@proton.me>
2023-01-30 16:09:30 +0100

    Revert "TP1803: Preopt SystemUI for AOSP builds." https://github.com/LineageOS/android_vendor_lineage/commit/7c9c674bc1b710d6a5f08922770d1ba698d30c9b
    This reverts commit f8061ca05b6c537e3eaea0693471effec7ab55e3.

--- device.mk
+++ device.mk
@@ -105,10 +105,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     disable_configstore
 
-# Dexpreopt
-PRODUCT_DEXPREOPT_SPEED_APPS += \
-    SystemUI
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
