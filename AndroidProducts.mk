04066a4692d1e6e5e716348baded323ae7de1a88
SirRGB <sirrgb@proton.me>
2023-07-23 14:35:28 +0200

    TP1803: bringup leaf

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_TP1803.mk
+    $(LOCAL_DIR)/TP1803.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_TP1803-user \
-    lineage_TP1803-userdebug \
-    lineage_TP1803-eng
+    TP1803-user \
+    TP1803-userdebug \
+    TP1803-eng
