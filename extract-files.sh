edb57f97de3e74eb218651a3ca5bd89fc08b91b4
Arian <arian.kulmer@web.de>
2024-10-26 16:06:11 +0200

    TP1803: Shim libwvdrmengine.so with libcrypto_shim too
    Change-Id: I0dade41d247900f1c15cff5211103d1a6a10dd3e

--- extract-files.sh
+++ extract-files.sh
@@ -78,7 +78,7 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             sed -i 's|libsnsapi.so|libsnsv28.so|g' "${2}"
             ;;
-        vendor/lib64/libwvhidl.so)
+        vendor/lib64/libwvhidl.so | vendor/lib64/mediadrm/libwvdrmengine.so)
             [ "$2" = "" ] && return 0
             grep -q libcrypto_shim.so "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
             ;;
